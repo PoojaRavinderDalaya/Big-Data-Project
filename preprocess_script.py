8e2a37f619a44e3cc1a6d1fce73955047dcaf735
sbeesabathuni <sbeesabathun@cs.stonybrook.edu>
2017-12-15 21:05:05 -0500

    Common cause - Chronic Respiratory Disease

--- preprocess_script.py
+++ preprocess_script.py
@@ -2,24 +2,11 @@ from flask import Flask
 import pandas as pd
 
 app = Flask(__name__)
-def CSVForTimeSeries():
-    df = pd.read_csv(app.root_path + "\\..\\data\\merged_final_Sravya.csv")
-    df = df.loc[df['sex'] == 'Both']
-    requiredColumns = []
-    requiredColumns.append('location_name')
-    requiredColumns.append('year_id')
-    #requiredColumns.append('FIPS')
-    requiredColumns.append('cancer_mx')
-    new_df = df[requiredColumns]
-
-    print(new_df.columns)
-    new_df.to_csv(app.root_path + '\\..\\data\\test_cancer.csv', index=False)
-
-
-    df = pd.read_csv(app.root_path + "\\..\\data\\test_cancer.csv")
+def CSVForPivotYears():
+    df = pd.read_csv(app.root_path + "/Files/test_ChronicRespiratory.csv")
     new_df = df.drop_duplicates(['location_name', 'year_id'])
-    new_df_2 = pd.pivot_table(new_df,index='year_id', columns='location_name', values='cancer_mx')
-    new_df_2.to_csv(app.root_path + "\\..\\data\\test_cancer_final.csv")
+    new_df_2 = pd.pivot_table(new_df,index='location_name', columns='year_id', values='Chronic respiratory diseases_mx')
+    new_df_2.to_csv(app.root_path + "/Files/test_chronic_final.csv")
 
 if __name__ =="__main__":
-    CSVForTimeSeries()
+    CSVForPivotYears()
